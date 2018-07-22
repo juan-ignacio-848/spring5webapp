2f6bf60850d689685e966d70d6235bc3b8d982a8
nmkip <juan.ignacio848@gmail.com>
2018-07-22 15:59:48 -0300

    Added publisher entity. Modified Book and DevBootstrap

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -16,14 +18,16 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
     // We can use @Autowired or we can create a constructor to have the repositories injected.
     // This class will get created and managed by the Spring Framework and the Author and Book
     // Repository (which we are going to get an implemention of from Spring Data JPA) are going
     // to get autowired into this class.
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -34,19 +38,23 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     private void initData() {
         // Eric
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Publisher harperCollins = new Publisher("Harper Collins", "Rivadavia 323");
+        Book ddd = new Book("Domain Driven Design", "1234", harperCollins);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
         authorRepository.save(eric);
+        publisherRepository.save(harperCollins);
         bookRepository.save(ddd);
 
         // Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        Publisher worx = new Publisher("Worx", "Corrientes 124");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", worx);
         rod.getBooks().add(noEJB);
 
         authorRepository.save(rod);
+        publisherRepository.save(worx);
         bookRepository.save(noEJB);
     }
 
